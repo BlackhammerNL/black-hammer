e9fc5e0fdc0408197ce32ecb909ad2b4630b5687
Fabian Williams <fabianwilliams@Mac.home>
2026-07-02 01:22:12 +0200

    Hide portfolio section until real photos are ready

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Hero />
         <Benefits />
         <Services />
-        <Portfolio />
+        {/* <Portfolio /> */}
         <Process />
         <About />
         <Faq />
